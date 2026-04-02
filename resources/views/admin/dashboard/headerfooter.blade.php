e3a00dd00e6878db10f7c6d8ddd13ab37b303721
jcitso nazeer <jcitsonazeer@gmail.com>
2026-04-02 12:11:51 +0530

    telescope-corrected

--- resources/views/admin/dashboard/headerfooter.blade.php
+++ resources/views/admin/dashboard/headerfooter.blade.php
@@ -14,6 +14,7 @@
     <!-- Master Stylesheet CSS -->
     <link rel="stylesheet" href="{{ asset('assets/admin/css/style.css') }}">
 
+
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     @livewireStyles
